f8a6f9053a42b15f72cd35b714e04ae38f1bda79
lochangit <lochan.vasudev@gmail.com>
2021-07-27 23:00:30 +0200

    test load 3

--- sa_abinitio_code_deploy.groovy
+++ sa_abinitio_code_deploy.groovy
@@ -1,13 +1,20 @@
 pipeline {
   agent any
   parameters {
-    string(name: 'STATEMENT', defaultValue: 'hello; ls /', description: 'What should I say?')
+    string(name: 'ABI_BUILD_TASK_ID', defaultValue: 'Task Id', description: 'Build task id. Format:Userstory_TaskId, Userstory_DefectId')
+    choice(name: 'ABI_BUILD_ENV', choices: ['SIT', 'UAT', 'PROD'], description: 'Build environment')
+    choice(name: 'ABI_BUILD_DOMAIN', choices: ['con', 'com', 'ids', 'cnc', 'ufo', 'uda'], description: 'Build domain name')
+    string(name: 'ABI_BUILD_BRANCH', defaultValue: 'main', description: 'Abinitio eme branch')
+    choice(name: 'ABI_TAG_SCOPE', choices: ['exact', 'project', 'eme'], description: 'Abinitio tag scope')
+    booleanParam(name: 'ABI_MAJOR_RELEASE', defaultValue: false, description: 'Is major code release?')
+    choice(name: 'ABI_TAG_TYPE', choices: ['build', 'test', 'backup', 'super'], description: 'Abinitio tag type')
+    text(name: 'ABI_TAG_OBJECTS', defaultValue: '', description: 'Enter the objects to tag')
   }
   stages {
     stage('Example') {
       steps {
         /* WRONG! */
-        sh("echo ${STATEMENT}")
+        sh("echo ${ABI_BUILD_TASK_ID}")
       }
     }
   }
