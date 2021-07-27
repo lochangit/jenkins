445aac78329f9d084e46316b7b55195eaabdfa5f
lochangit <lochan.vasudev@gmail.com>
2021-07-28 00:19:28 +0200

    test load 4

--- sa_abinitio_code_deploy.groovy
+++ sa_abinitio_code_deploy.groovy
@@ -1,10 +1,21 @@
+def ab_eme_branch_list = []
+
+node('master') {
+   stage('get active branches') {
+       // get branch list
+       def ab_eme_branches = sh script: "ls -l /home", returnStdout:true
+       ab_eme_branch_list = ab_eme_branches.trim().tokenize("\n")
+   }
+}
+
+
 pipeline {
   agent any
   parameters {
     string(name: 'ABI_BUILD_TASK_ID', defaultValue: 'Task Id', description: 'Build task id. Format:Userstory_TaskId, Userstory_DefectId')
     choice(name: 'ABI_BUILD_ENV', choices: ['SIT', 'UAT', 'PROD'], description: 'Build environment')
     choice(name: 'ABI_BUILD_DOMAIN', choices: ['con', 'com', 'ids', 'cnc', 'ufo', 'uda'], description: 'Build domain name')
-    string(name: 'ABI_BUILD_BRANCH', defaultValue: 'main', description: 'Abinitio eme branch')
+    choice(name: 'ABI_BUILD_BRANCH', choices: ab_eme_branch_list, description: 'Abinitio eme branch')
     choice(name: 'ABI_TAG_SCOPE', choices: ['exact', 'project', 'eme'], description: 'Abinitio tag scope')
     booleanParam(name: 'ABI_MAJOR_RELEASE', defaultValue: false, description: 'Is major code release?')
     choice(name: 'ABI_TAG_TYPE', choices: ['build', 'test', 'backup', 'super'], description: 'Abinitio tag type')
