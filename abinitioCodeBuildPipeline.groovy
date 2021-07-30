37627369a5519fa2c0a57aa945c877b4a4aa19d8
lochangit <lochan.vasudev@gmail.com>
2021-07-30 20:57:29 +0200

    test load 8

--- abinitioCodeBuildPipeline.groovy
+++ abinitioCodeBuildPipeline.groovy
@@ -19,10 +19,11 @@ pipeline {
   parameters {
     booleanParam(name: 'REFRESH_PIPELINE', defaultValue: false, description: 'Refresh pipeline without build?')
     string(name: 'ABI_BUILD_TASK_ID', defaultValue: 'Task Id', description: 'Build task id. Format:Userstory_TaskId, Userstory_DefectId')
+    string(name: 'ABI_BUILD_COMMENT', defaultValue: 'Task Id', description: 'Build task id. Format:Userstory_TaskId, Userstory_DefectId')
     choice(name: 'ABI_BUILD_BRANCH', choices: ab_eme_branch_list, description: 'Abinitio eme branch')
     choice(name: 'ABI_CHECKOUT_ENV', choices: ['BATCH', 'ONLINE', 'BATCH_AND_ONLINE'], description: 'Code checkout environment')
     choice(name: 'ABI_BUILD_DOMAIN', choices: ['con', 'com', 'ids', 'cnc', 'ufo', 'uda'], description: 'Build domain name')
-    choice(name: 'ABI_RELEASE_SCOPE', choices: ['major', 'minor', 'patch'], description: 'Code release scope')
+    choice(name: 'ABI_RELEASE_SCOPE', choices: ['minor', 'patch', 'major'], description: 'Code release scope')
     choice(name: 'ABI_TAG_SCOPE', choices: ['exact', 'project', 'eme'], description: 'Abinitio tag scope')
     choice(name: 'ABI_TAG_TYPE', choices: ['build', 'test', 'backup', 'super'], description: 'Abinitio tag type')
     text(name: 'ABI_TAG_OBJECTS', defaultValue: '', description: 'Enter the objects to tag')
@@ -52,9 +53,9 @@ pipeline {
             }
 
             	steps {
-                	script { 
+                	//script { 
                     		pipelineHelper.echoEnvVars()
-                	}
+                	//}
             	}
         }
     }
--- vars/abinitioPipelineHelper.groovy
+++ vars/abinitioPipelineHelper.groovy
@@ -2,9 +2,9 @@
 def echoEnvVars() {
 	CURR_JAVA_VERSION = sh "java --version"
 	CURR_HOST_NAME = sh "hostname"
-	echo "Build number : ${CURR_JAVA_VERSION}"
-	echo "Build number : ${CURR_HOST_NAME}"
-        echo "Build number : ${env.BUILD_NUMBER}"	
+	echo "Java version : ${CURR_JAVA_VERSION}"
+	echo "Host name : ${CURR_HOST_NAME}"
+        echo "Build id : ${env.BUILD_NUMBER}"	
 	echo "Current working dir : ${pwd()}"
     	echo "Path : ${env.PATH}"    
 }
