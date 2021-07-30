259ce996537c1077a65deb6c59e9dab89dd92cc5
lochangit <lochan.vasudev@gmail.com>
2021-07-31 01:57:58 +0200

    test load 1

--- abinitioCodeBuildPipeline.groovy
+++ abinitioCodeBuildPipeline.groovy
@@ -14,6 +14,7 @@ node('jenkins-agent-01') {
 }
 
 
+
 pipeline {
   agent any
   parameters {
@@ -35,11 +36,6 @@ pipeline {
     timestamps()
   }
 
-  environment {
-    //Use Pipeline Utility Steps plugin to read information from pom.xml into env variables
-    IMAGE = readMavenPom().getArtifactId()
-    VERSION = readMavenPom().getVersion()
-  }
 
     stages {
         stage('Initialize') {
--- vars/pipelineHelper.groovy
+++ vars/pipelineHelper.groovy
@@ -7,9 +7,5 @@ def echoEnvVars() {
         echo "Build number : ${env.BUILD_NUMBER}"	
 	echo "Current working dir : ${pwd()}"
     	echo "Path : ${env.PATH}"    
-    	//echo "Abinitio EME host : ${env.ABI_EME_HOST}" 
-        //echo "Abinitio EME root : ${env.ABI_EME_ROOT}" 
-	//echo "Working directory : ${env.BUILD_WORK_DIR}" 
-	//echo "Log directory : ${env.BUILD_LOG_DIR}"
 }
 
