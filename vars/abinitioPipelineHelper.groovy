5d07628b4c9256a3a955e8fa4685852dbf5250f7
lochangit <lochan.vasudev@gmail.com>
2021-08-01 22:54:39 +0200

    test load 1

--- vars/abinitioPipelineHelper.groovy
+++ vars/abinitioPipelineHelper.groovy
@@ -2,10 +2,11 @@
 def echoEnvVars() {
 	CURR_JAVA_VERSION = sh "java --version"
 	CURR_HOST_NAME = sh "hostname"
+        CURR_WORK_DIR = sh "pwd"
 	echo "Java version : ${CURR_JAVA_VERSION}"
 	echo "Host name : ${CURR_HOST_NAME}"
         echo "Build id : ${env.BUILD_NUMBER}"	
-	echo "Current working dir : ${pwd()}"
+	echo "Current working dir : ${CURR_WORK_DIR}"
     	echo "Path : ${env.PATH}"    
 }
 
