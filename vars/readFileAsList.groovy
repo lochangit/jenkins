f3a266fa0e9f56bacbcff254441555c50a899862
lochangit <lochan.vasudev@gmail.com>
2021-08-03 23:09:46 +0200

    test load 1

--- vars/readFileAsList.groovy
+++ vars/readFileAsList.groovy
@@ -1,6 +1,6 @@
 def call(Map fileName = [:]) { 
   def fileContents = libraryResource "${fileName.name}"    
-  return scriptcontents.replaceAll("(?m)^[ \t]*\r?\n", "").split('\n') 
+  return fileContents.replaceAll("(?m)^[ \t]*\r?\n", "").split('\n') 
 } 
 
 
--- vars/readFileAsListHelper.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map fileName = [:]) { 
-  def fileContents = libraryResource "${fileName.name}"    
-  return fileContents.replaceAll("(?m)^[ \t]*\r?\n", "").split('\n') 
-} 
-
-
-
-
