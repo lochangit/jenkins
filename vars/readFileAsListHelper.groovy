43062db1aa8836e9739110cc3190d670373ab65e
lochangit <lochan.vasudev@gmail.com>
2021-08-03 22:58:04 +0200

    test load 1

--- vars/readFileAsListHelper.groovy
+++ vars/readFileAsListHelper.groovy
@@ -1,6 +1,6 @@
 def call(Map fileName = [:]) { 
   def fileContents = libraryResource "${fileName.name}"    
-  sh "echo ${ABI_ENV_EME_HOST}"
+  echo fileContents
   //return scriptcontents.replaceAll("(?m)^[ \t]*\r?\n", "").split('\n') 
 } 
 
