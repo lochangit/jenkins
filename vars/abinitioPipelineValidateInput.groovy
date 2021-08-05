dd5db277aa59e3d115889a48919a678c4a34d8d1
lochangit <lochan.vasudev@gmail.com>
2021-08-06 01:08:26 +0200

    test load 1

--- vars/abinitioPipelineValidateInput.groovy
+++ vars/abinitioPipelineValidateInput.groovy
@@ -5,7 +5,7 @@
 def call(Map inputParams = [:]) {
 
 	// Validation for task ID
-    	if( !(inputParams.taskId ==~ /[A-Z]{2}d{4}_[A-Z]{2}d{4})/) ){
+    	if( !(inputParams.taskId ==~ /[A-Z]{2}d{4}_[A-Z]{2}d{4})/ ){
         	 throw new Exception("Invalid input task id : ${inputParams.taskId}. Correct and resubmit build") 
     	}
 }
