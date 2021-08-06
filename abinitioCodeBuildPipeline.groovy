814e2f947eef73aa465e49f023ee64faf23057cd
lochangit <lochan.vasudev@gmail.com>
2021-08-06 21:13:14 +0200

    test load 1

--- abinitioCodeBuildPipeline.groovy
+++ abinitioCodeBuildPipeline.groovy
@@ -22,8 +22,8 @@ pipeline {
   // Build input parameters to be supplied by user
   parameters {
     booleanParam(name: 'REFRESH_PIPELINE', defaultValue: false, description: 'Refresh pipeline without build?')
-    string(name: 'ABI_BUILD_TASK_ID', defaultValue: 'Task Id', description: 'Build task id. Format:Userstory_TaskId, Userstory_DefectId')
-    string(name: 'ABI_BUILD_COMMENT', defaultValue: 'Task Id', description: 'Build task id. Format:Userstory_TaskId, Userstory_DefectId')
+    string(name: 'ABI_BUILD_TASK_ID', defaultValue: 'Task Id', description: 'Build task id. Format:USNNNN_TKNNNN, USNNNN_DENNNN')
+    string(name: 'ABI_BUILD_COMMENT', defaultValue: 'Task Id', description: 'Release comments')
     choice(name: 'ABI_BUILD_BRANCH', choices: ab_eme_branch_list, description: 'Abinitio eme branch')
     choice(name: 'ABI_CHECKOUT_ENV', choices: ['batch', 'online', 'batch_and_online'], description: 'Code checkout environment')
     choice(name: 'ABI_BUILD_DOMAIN', choices: ['con', 'com', 'ids', 'cnc', 'ufo', 'uda'], description: 'Build domain name')
@@ -46,7 +46,9 @@ pipeline {
                 	message "Continue with build?"
                 	ok "Yes"
                 	parameters {
+			string(name: 'REFRESH_PIPELINE', defaultValue: params.REFRESH_PIPELINE)
                     	string(name: 'ABI_BUILD_TASK_ID', defaultValue: params.ABI_BUILD_TASK_ID)
+			string(name: 'ABI_BUILD_COMMENT', defaultValue: params.ABI_BUILD_COMMENT)
 			string(name: 'ABI_BUILD_BRANCH', defaultValue: params.ABI_BUILD_BRANCH)
 			string(name: 'ABI_CHECKOUT_ENV', defaultValue: params.ABI_CHECKOUT_ENV)
 			string(name: 'ABI_BUILD_DOMAIN', defaultValue: params.ABI_BUILD_DOMAIN)
@@ -54,6 +56,7 @@ pipeline {
 			string(name: 'ABI_TAG_SCOPE', defaultValue: params.ABI_TAG_SCOPE)
 			string(name: 'ABI_TAG_TYPE', defaultValue: params.ABI_TAG_TYPE)
 			booleanParam(name: 'FORCE_BUILD', defaultValue: params.FORCE_BUILD)
+			string(name: 'ABI_TAG_OBJECTS', defaultValue: params.ABI_TAG_OBJECTS)
                 	}
             }
 
@@ -71,7 +74,15 @@ pipeline {
 
         		success {
             			echo 'Initialization complete with following parameters : '
+				echo "REFRESH_PIPELINE ${params.REFRESH_PIPELINE}.toString()"
                     		echo "ABI_BUILD_TASK_ID ${params.ABI_BUILD_TASK_ID}"
+				echo "ABI_BUILD_BRANCH ${params.ABI_BUILD_BRANCH}"
+				echo "ABI_CHECKOUT_ENV ${params.ABI_CHECKOUT_ENV}"
+				echo "ABI_BUILD_DOMAIN ${params.ABI_BUILD_DOMAIN}"
+				echo "ABI_RELEASE_SCOPE ${params.ABI_RELEASE_SCOPE}"
+				echo "ABI_TAG_SCOPE ${params.ABI_TAG_SCOPE}"
+				echo "ABI_TAG_TYPE ${params.ABI_TAG_TYPE}"
+				echo "FORCE_BUILD ${params.FORCE_BUILD}.toString()"
         		}
 
         		failure {
