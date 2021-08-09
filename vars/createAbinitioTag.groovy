c381cba5e07acb29deb12f596314190c9640276c
lochangit <lochan.vasudev@gmail.com>
2021-08-09 17:36:28 +0200

    test load 1

--- vars/createAbinitioTag.groovy
+++ vars/createAbinitioTag.groovy
@@ -1,6 +1,7 @@
 def call(Map tagInput = [:]) {
     loadPythonScript(name: 'createAbinitioTag.py')
-    sh(returnStdout: true, script: "./createAbinitioTag.py ${tagInput.taskId} ${tagInput.taskComments} ${tagInput.buildBranch} ${tagInput.buildDomain} ${tagInput.releaseScope} ${tagInput.tagScope} ${tagInput.tagType} ${tagInput.objectList}")
+    tagName = sh(returnStdout: true, script: "./createAbinitioTag.py ${tagInput.taskId} ${tagInput.taskComments} ${tagInput.buildBranch} ${tagInput.buildDomain} ${tagInput.releaseScope} ${tagInput.tagScope} ${tagInput.tagType} ${tagInput.objectList}")
+  echo "Created tag successfully ${tagName}"
 }
 
 
