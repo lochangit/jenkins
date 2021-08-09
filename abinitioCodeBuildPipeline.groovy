9dc74b9b1f8e9ad7fdcea1f040f31486e14b9896
lochangit <lochan.vasudev@gmail.com>
2021-08-09 16:30:45 +0200

    test load 1

--- abinitioCodeBuildPipeline.groovy
+++ abinitioCodeBuildPipeline.groovy
@@ -126,16 +126,16 @@ pipeline {
 
   		// Set additional environment variables from configuration files based on agent name
   		environment {
-		env.ABI_ENV_EME_HOST = getEnvVariables(nodeName : env.NODE_NAME, variableName : "ABI_ENV_EME_HOST")
-		env.ABI_ENV_EME_ROOT = getEnvVariables(nodeName : env.NODE_NAME, variableName : "ABI_ENV_EME_ROOT")
-		env.ABI_ENV_AB_HOME  = getEnvVariables(nodeName : env.NODE_NAME, variableName : "ABI_ENV_AB_HOME")
+			ABI_ENV_EME_HOST = getEnvVariables(nodeName : env.NODE_NAME, variableName : "ABI_ENV_EME_HOST")
+			ABI_ENV_EME_ROOT = getEnvVariables(nodeName : env.NODE_NAME, variableName : "ABI_ENV_EME_ROOT")
+			ABI_ENV_AB_HOME  = getEnvVariables(nodeName : env.NODE_NAME, variableName : "ABI_ENV_AB_HOME")
   		}
 
             	steps {
 			echo "Creating tag with input parameters..."
 
 			// Create tag
-			env.ABI_TAG_NAME = createAbinitioTag(taskId		: params.ABI_BUILD_TASK_ID, 
+			ABI_TAG_NAME = createAbinitioTag(taskId		: params.ABI_BUILD_TASK_ID, 
 					  		     taskComments 	: params.ABI_BUILD_COMMENT,
 					  		     buildBranch 	: params.ABI_BUILD_BRANCH,
 					   		     buildDomain 	: params.ABI_BUILD_DOMAIN,
