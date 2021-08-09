ceb50882c04c75c285ae3214a3a97eac2ccc8864
lochangit <lochan.vasudev@gmail.com>
2021-08-09 18:06:46 +0200

    test load 1

--- vars/createAbinitioTag.groovy
+++ vars/createAbinitioTag.groovy
@@ -1,7 +1,7 @@
 def call(Map tagInput = [:]) {
     loadPythonScript(name: 'createAbinitioTag.py')
     tagName = sh(returnStdout: true, script: "./createAbinitioTag.py ${tagInput.taskId} ${tagInput.taskComments} ${tagInput.buildBranch} ${tagInput.buildDomain} ${tagInput.releaseScope} ${tagInput.tagScope} ${tagInput.tagType} ${tagInput.objectList}")
-  echo "Created tag successfully ${tagName}"
+  echo "Created tag successfully ${tagInput.tagNames}"
 }
 
 
--- vars/getAbinitioTagName.groovy
+++ vars/getAbinitioTagName.groovy
@@ -1,6 +1,6 @@
 def call(Map tagInput = [:]) {
     loadPythonScript(name: 'createAbinitioTag.py')
-    tagName = sh(returnStdout: true, script: "echo ${tagInput.buildDomain}.project_name.${tagInput.buildBranch}.${tagInput.taskId}.${tagInput.tagScope}.101.10.20210202.121212.${tagInput.tagType}")
+    sh(returnStdout: true, script: "${tagInput.buildDomain}.project_name.${tagInput.buildBranch}.${tagInput.taskId}.${tagInput.tagScope}.101.10.20210202.121212.${tagInput.tagType}")
 }
 
 
